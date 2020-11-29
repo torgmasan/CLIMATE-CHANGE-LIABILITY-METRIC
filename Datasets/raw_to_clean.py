37027207e3453dce3c98ac6ca3bbbd7f1207a9f6
yde773786 <yde773786@gmail.com>
2020-11-30 03:18:37 +0400

    raw_to_clean.py completed

--- Datasets/raw_to_clean.py
+++ Datasets/raw_to_clean.py
@@ -1,16 +1,27 @@
-from typing import List
+from typing import Dict
 import csv
+from dataclasses import dataclass
 
 
-def extract_wanted_column(file_name: str, first_column_name: str, second_column_name) -> List[List[str]]:
-    """Return two lists owhich contain the essential columns from the input csv files for further processing.
+@dataclass
+class Country:
+    """Class for maintaining information about each country"""
+    name: str
+    co2_emissions: float
+    gdp_per_capita: float
+    renewable_energy_percentage: float
+    cri_rank: float
+    budget_allotted: float
+
+
+def extract_wanted_column(file_name: str, first_column_name: str, second_column_name) -> Dict[str, str]:
+    """Return two lists on which contain the essential columns from the input csv files for further processing.
 
     Precondition:
         - filepath refers to a csv file with 2 or more columns
         - first and second column strings are present in the csv header
     """
-    each_row_req_1 = []
-    each_row_req_2 = []
+    mapping_of_relevant_columns = {}
 
     with open(file_name) as csvfile:
         reader = csv.reader(csvfile)
@@ -25,7 +36,62 @@ def extract_wanted_column(file_name: str, first_column_name: str, second_column_
                     req_j = j
 
         for row in reader:
-            each_row_req_1.append(row[req_i])
-            each_row_req_2.append(row[req_j])
+            if row[req_j] != '':
+                mapping_of_relevant_columns[row[req_i]] = row[req_j]
+            else:
+                mapping_of_relevant_columns[row[req_i]] = -999
 
-    return [each_row_req_1, each_row_req_2]
+    return mapping_of_relevant_columns
+
+
+COUNTRY_CODE_TABLE = extract_wanted_column('./Raw Datasets/countries_codes_and_coordinates.csv',
+                                           'Alpha-3 code', 'Country', )
+
+
+def iso_to_name(iso_target: str) -> str:
+    """Converts the input iso to give to corresponding country name
+
+    Precondition:
+        iso entered is valid
+    """
+    if iso_target in COUNTRY_CODE_TABLE:
+        return COUNTRY_CODE_TABLE[iso_target]
+    else:
+        return 'Not Found'
+
+
+def create_dataset() -> Dict[str, Country]:
+    """Provides a dictionary mapping the ISO codes to the corresponding country
+    dataclass. The dataclass provided has no budget of 0 when initialized.
+
+    Precondition:
+        All csv files contain valid information
+    """
+
+    dataset_dict = {}
+
+    co2_emissions_data = extract_wanted_column('./Raw Datasets/co2_emissions.csv',
+                                               'Country Code', '2014', )
+    gdp_per_capita_data = extract_wanted_column('./Raw Datasets/gdp.csv',
+                                                'Country Code', '2014', )
+    renewable_energy_percentage_data = extract_wanted_column('./Raw Datasets/Renewable Energy.csv',
+                                                             'Country Code', '2014', )
+
+    climate_risk_index_data = extract_wanted_column('./Raw Datasets/cri.csv', 'Country Name', 'Score')
+
+    for iso in COUNTRY_CODE_TABLE:
+        name = COUNTRY_CODE_TABLE[iso]
+
+        try:
+            co2_emissions = float(co2_emissions_data[iso])
+            gdp_per_capita = float(gdp_per_capita_data[iso])
+            renewable_energy_percentage = float(renewable_energy_percentage_data[iso])
+            climate_risk_index = float(climate_risk_index_data[iso_to_name(iso)])
+
+            dataset_dict[iso] = Country(name, co2_emissions, gdp_per_capita, renewable_energy_percentage,
+                                        climate_risk_index, 0)
+
+        except KeyError:
+            print('LOG: unavailable data for ' + COUNTRY_CODE_TABLE[iso])
+
+    return dataset_dict
