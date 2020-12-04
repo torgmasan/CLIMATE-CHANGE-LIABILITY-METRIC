3f1a9ec8821e618e6181d6244f64ac9385ec9372
yde773786 <yde773786@gmail.com>
2020-12-04 11:55:43 +0400

    minor changes to Computation.py

--- Computation.py
+++ Computation.py
@@ -1,6 +1,5 @@
-import raw_to_clean
+from Datasets.raw_to_clean import Country, get_datasets
 from typing import Dict, List, Set
-import statistics
 import math
 """ Computation code:
                 TODO LIST:
@@ -24,7 +23,7 @@ def check_factor(factor: str, proportionality: str) -> None:
 def calculate_total(factor: str, year: str) -> float:
     """Calculates the total of the factor passed"""
     total = 0.0
-    data = raw_to_clean.get_datasets(year)
+    data = get_datasets(year)
     for countries in data:
         total += data[countries]
     return total
@@ -33,7 +32,7 @@ def calculate_total(factor: str, year: str) -> float:
 def positive_calculation(factor: str, year: str, country: str) -> float:
     """Calculates the weighted responsibility if the relation is positive."""
     total_data = calculate_total(factor, year)
-    country_data = float(raw_to_clean.get_datasets(year)[factor][country])
+    country_data = float(get_datasets(year)[factor][country])
     calc = country_data / total_data * 100
     return calc
 
@@ -41,15 +40,15 @@ def positive_calculation(factor: str, year: str, country: str) -> float:
 def negative_calculation(factor: str, year: str, country: str) -> float:
     """Calculates the weighted responsibility if the relation is negative."""
     total_data = calculate_total(factor, year)
-    country_data = float(raw_to_clean.get_datasets(year)[factor][country])
-    sum_so_far = sum([total_data - float(raw_to_clean.get_datasets(year)[factor][i]) for i in
-                      raw_to_clean.get_datasets(year)[factor]])
+    country_data = float(get_datasets(year)[factor][country])
+    sum_so_far = sum([total_data - float(get_datasets(year)[factor][i]) for i in
+                      get_datasets(year)[factor]])
     calc = (total_data - country_data) / sum_so_far * 100
     return calc
 
 
 def responsibility(a: float, b: float, c: float, d: float,
-                   country: raw_to_clean.Country) -> float:
+                   country: Country) -> float:
     """Calculates the responsibility of the given country.
 
     Preconditions:
