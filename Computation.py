22bd0c1b8ef967dbeeaf8ebea4f7e720b467431f
sidsachdev12 <sid.sachdev@mail.utoronto.ca>
2020-12-04 11:42:32 +0400

    check factor completed. calculate total completed. positive and negative calculations completed. need to do responsibility.

--- Computation.py
+++ Computation.py
@@ -1,3 +1,61 @@
-import Datasets.raw_to_clean
+import raw_to_clean
+from typing import Dict, List, Set
+import statistics
+import math
+""" Computation code:
+                TODO LIST:
+                    1. Create a dataclass with some instance variables that will be changed.
+                    4. Return a dictionary containing the gdp% as well as other factors
+                    5. -999 to be distributed to others
+                    6. a, b, c, d weights to be included as parameters
+                    7. Make the factor dictionary positive or negative.
+                    8. Take year as an input.
+"""
+factor_proportionality = {'Renewable Energy': 'inverse', 'co2_emissions': 'direct', 'cri': 'direct',
+                          'gdp_per_capita': 'direct'}
 
-"""Write computation here."""
+
+def check_factor(factor: str, proportionality: str) -> None:
+    """Checks whether a factor is already in the factor_proportionality dict and adds if not"""
+    if factor not in factor_proportionality:
+        factor_proportionality[factor] = proportionality
+
+
+def calculate_total(factor: str, year: str) -> float:
+    """Calculates the total of the factor passed"""
+    total = 0.0
+    data = raw_to_clean.get_datasets(year)
+    for countries in data:
+        total += data[countries]
+    return total
+
+
+def positive_calculation(factor: str, year: str, country: str) -> float:
+    """Calculates the weighted responsibility if the relation is positive."""
+    total_data = calculate_total(factor, year)
+    country_data = float(raw_to_clean.get_datasets(year)[factor][country])
+    calc = country_data / total_data * 100
+    return calc
+
+
+def negative_calculation(factor: str, year: str, country: str) -> float:
+    """Calculates the weighted responsibility if the relation is negative."""
+    total_data = calculate_total(factor, year)
+    country_data = float(raw_to_clean.get_datasets(year)[factor][country])
+    sum_so_far = sum([total_data - float(raw_to_clean.get_datasets(year)[factor][i]) for i in
+                      raw_to_clean.get_datasets(year)[factor]])
+    calc = (total_data - country_data) / sum_so_far * 100
+    return calc
+
+
+def responsibility(a: float, b: float, c: float, d: float,
+                   country: raw_to_clean.Country) -> float:
+    """Calculates the responsibility of the given country.
+
+    Preconditions:
+        - a + b + c + d = 1
+        - proportion == 'inverse' or relation == 'direct'
+    """
+    weighted_result = 0.0
+
+    return weighted_result
