0c0fd04d380375c4995d8af8c37e7f7c26b5b781
sidsachdev12 <sid.sachdev@mail.utoronto.ca>
2020-12-09 03:31:18 +0400

    Responsibility now returns the weighted average instead of weighted sum. All methods besides check_factor, responsibility, budget_percentage are private.

--- Datasets/computation.py
+++ Datasets/computation.py
@@ -19,7 +19,7 @@ def check_factor(factor: str, proportionality: str) -> None:
         factor_proportionality[factor] = proportionality
 
 
-def calculate_total(factor: str, year: str) -> float:
+def _calculate_total(factor: str, year: str) -> float:
     """Calculates the total of the factor passed"""
     total = 0.0
     data = get_datasets(year)
@@ -30,18 +30,18 @@ def calculate_total(factor: str, year: str) -> float:
     return total
 
 
-def positive_calculation(factor: str, year: str, country: Country) -> float:
+def _positive_calculation(factor: str, year: str, country: Country) -> float:
     """Calculates the weighted responsibility if the relation is positive."""
-    total_data = calculate_total(factor, year)
+    total_data = _calculate_total(factor, year)
     country_data = country.factors[factor]
     calc = country_data / total_data * 100
 
     return calc
 
 
-def negative_calculation(factor: str, year: str, country: Country) -> float:
+def _negative_calculation(factor: str, year: str, country: Country) -> float:
     """Calculates the weighted responsibility if the relation is negative."""
-    total_data = calculate_total(factor, year)
+    total_data = _calculate_total(factor, year)
     country_data = country.factors[factor]
     sum_so_far = sum([total_data - float(get_datasets(year)[factor][i]) for i in
                       get_datasets(year)[factor]])
@@ -50,7 +50,7 @@ def negative_calculation(factor: str, year: str, country: Country) -> float:
     return calc
 
 
-def unavailable_value(country: Country, weights: Dict[str, float]) -> float:
+def _unavailable_value(country: Country, weights: Dict[str, float]) -> float:
     """Calculates the split of the weights for responsibility based on the number of\
     unavailable values"""
     count_so_far = 0
@@ -78,21 +78,22 @@ def responsibility(weights: Dict[str, float],
     """
     weighted_result = 0.0
     score = {}
-    split = unavailable_value(country, weights)
-
+    split = _unavailable_value(country, weights)
+    total = 0
     for factor in country.factors:
         if country.factors[factor] != -999:
             if factor_proportionality[factor] == 'direct':
-                result = positive_calculation(factor, year, country)
+                result = _positive_calculation(factor, year, country)
                 score[factor] = result
             else:
-                result = negative_calculation(factor, year, country)
+                result = _negative_calculation(factor, year, country)
                 score[factor] = result
-
+    for value in weights:
+        total += weights[value]
     for factor in country.factors:
         weighted_result += score[factor] * (weights[factor] + split)
 
-    return weighted_result
+    return weighted_result / total
 
 
 def budget_percentage(total_budget: float, country: Country,
