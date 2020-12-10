a2e531fe7c21bee42e055ffb55ef362d089ebb8d
yde773786 <yde773786@gmail.com>
2020-12-10 11:33:03 +0400

    revise computation.py and add a tester method (temporary)

--- Datasets/computation.py
+++ Datasets/computation.py
@@ -1,40 +1,53 @@
 from Datasets.dataset_utilities import Country, get_clean_datasets
 from typing import Dict
 
+CLEAN_DATASET = {}
 
-def _calculate_total(factor: str, year: str) -> float:
+
+def set_up_computation(year: str) -> None:
+    """Decrease computation time by calling the clean
+    dataset once and providing th value to global constant
+    
+    Precondition:
+        - Must be called before any computation is done.
+    """
+    global CLEAN_DATASET
+    CLEAN_DATASET = get_clean_datasets(year)
+
+
+def _calculate_total(factor: str) -> float:
     """Calculates the total of the factor passed"""
     total = 0.0
-    data = get_clean_datasets(year)
 
-    for countries in data[factor]:
-        total += data[countries]
+    for countries in CLEAN_DATASET[factor]:
+        if CLEAN_DATASET[factor][countries] != -999:
+            total += CLEAN_DATASET[factor][countries]
 
     return total
 
 
-def _positive_calculation(factor: str, year: str, country: Country) -> float:
+def _positive_calculation(factor: str, country: Country) -> float:
     """Calculates the weighted responsibility if the relation is positive."""
-    total_data = _calculate_total(factor, year)
+    total_data = _calculate_total(factor)
     country_data = country.factors[factor]
     calc = country_data / total_data * 100
 
     return calc
 
 
-def _negative_calculation(factor: str, year: str, country: Country) -> float:
+def _negative_calculation(factor: str, country: Country) -> float:
     """Calculates the weighted responsibility if the relation is negative."""
-    total_data = _calculate_total(factor, year)
+    total_data = _calculate_total(factor)
     country_data = country.factors[factor]
-    sum_so_far = sum([total_data - float(get_clean_datasets(year)[factor][i]) for i in
-                      get_clean_datasets(year)[factor]])
+    sum_so_far = sum([total_data - float(CLEAN_DATASET[factor][i]) for i in
+                      CLEAN_DATASET[factor]])
     calc = (total_data - country_data) / sum_so_far * 100
 
     return calc
 
 
 def _unavailable_value(country: Country, weights: Dict[str, float]) -> float:
-    """Calculates the split of the weights for responsibility based on the number of\
+    """Calculates the split of the weights for responsibility based on the number of
     unavailable values"""
     count_so_far = 0
     split = 0.0
@@ -52,7 +65,7 @@ def _unavailable_value(country: Country, weights: Dict[str, float]) -> float:
 
 
 def responsibility(weights: Dict[str, float],
-                   country: Country, year: str) -> float:
+                   country: Country, factor_proportionality: Dict[str, str]) -> float:
     """Calculates the responsibility of the given country.
 
     Preconditions:
@@ -62,26 +75,35 @@ def responsibility(weights: Dict[str, float],
     weighted_result = 0.0
     score = {}
     split = _unavailable_value(country, weights)
-    total = 0
+
     for factor in country.factors:
         if country.factors[factor] != -999:
             if factor_proportionality[factor] == 'direct':
-                result = _positive_calculation(factor, year, country)
-                score[factor] = result
+                result = _positive_calculation(factor, country)
             else:
-                result = _negative_calculation(factor, year, country)
-                score[factor] = result
-    for value in weights:
-        total += weights[value]
-    for factor in country.factors:
+                result = _negative_calculation(factor, country)
+
+            score[factor] = result
+
+    for factor in score:
         weighted_result += score[factor] * (weights[factor] + split)
 
-    return weighted_result / total
+    return weighted_result
 
 
 def budget_percentage(total_budget: float, country: Country,
-                      year: str, weights: Dict[str, float]) -> float:
+                      factor_proportionality: Dict[str, str], weights: Dict[str, float]) -> float:
     """Calculates the budget and the budget percentage based on the responsibility of the country"""
-    budget = responsibility(weights, country, year) * total_budget
+    budget = responsibility(weights, country, factor_proportionality) * total_budget
     percentage = budget / country.gdp * 100
     return percentage
+
+
+def __test_country_responsibility__(country: Country):
+    set_up_computation('2014')
+    weights = {'GDP': 0.35, 'Climate Risk Index': 0.27, 'Renewable Energy': 0.16,
+               'Carbon Dioxide Emissions': 0.22}
+    factor_responsibility = {'GDP': 'direct', 'Climate Risk Index': 'direct', 'Renewable Energy': 'inverse',
+                             'Carbon Dioxide Emissions': 'direct'}
+
+    return responsibility(weights, country, factor_responsibility)
